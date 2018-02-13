206194d3ec0a0b4686a8ad35542e0e9b8a0816e5
LACY HOGAN <lacy.hogan@gmail.com>
2018-02-12 18:40:15 -0800

    Completed Pike stats and updated sales.html

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var times = [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-//var stores = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+var times = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var stores = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
 
 var firstAndPike = {
+  name: stores[0],
   minCustPerHour: 23,
   maxCustPerHour: 65,
   avgCookiesPerCust: 6.3,
@@ -11,23 +12,37 @@ var firstAndPike = {
   calcCustEachHour: function() {
     for(var i = 0; i < times.length; i++) {
       this.custEachHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+      console.log(this.custEachHour);
     }
   },
   cookiePerHour: [],
   calcCookiePerHour: function() {
+    this.calcCustEachHour();
     for(var i = 0; i < this.custEachHour.length; i++) {
       this.cookiePerHour.push(this.avgCookiesPerCust * this.custEachHour[i]);
+      console.log(this.cookiePerHour);
     }
   },
   totalCookiesPerDay: 0,
   calcTotalCookiePerDay: function() {
     this.calcCookiePerHour();
     for (var i = 0; i < this.cookiePerHour.length; i++) {
-      Number(this.totalCookiesPerDay += this.cookiePerHour[i]);//figure out how to round
+      this.totalCookiesPerDay += this.cookiePerHour[i];
+      console.log(this.totalCookiesPerDay);
     }
+  },
+  render: function() {
+    this.calcTotalCookiePerDay();
+    var ulEl = document.getElementById('firstAndPike');
+    for (var i = 0; i < times.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = times[i] + ': ' + Math.round(this.cookiePerHour[i]) + ' cookies';
+      ulEl.appendChild(liEl);
+    }
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + Math.round(this.totalCookiesPerDay) + 'cookies';
+    ulEl.appendChild(liEl);
   }
 };
 
-firstAndPike.calcCustEachHour();
-firstAndPike.calcCookiePerHour();
-firstAndPike.calcTotalCookiePerDay();
+firstAndPike.render();
