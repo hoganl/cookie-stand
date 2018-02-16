cc598ecf3c95a4e4c08315266ebbe33d687d3db8
LACY HOGAN <lacy.hogan@gmail.com>
2018-02-15 16:46:39 -0800

    Added footer to app.js, added all locations to index.html, and updated style.css to add style to index.html table

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var stores = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+Store.allStores = [];
 
 var storeTable = document.getElementById('storeCookies');
 
@@ -13,7 +14,7 @@ function Store (storeName, minCustPerHour, maxCustPerHour, avgCookiesPerCust) {
   this.custEachHour = [];
   this.cookiesPerHour = [];
   this.totalCookiesPerDay = 0;
-  allStores.push(this);
+  Store.allStores.push(this);
   this.calcCustEachHour();
   this.calcCookiesPerHour();
   this.calcTotalCookiesPerDay();
@@ -54,22 +55,20 @@ Store.prototype.render = function() {
   }
 
   tdEl = document.createElement('td');
-  tdEl.className = 'totalsCell';
+  tdEl.id = 'totalsCell';
   tdEl.textContent = this.totalCookiesPerDay;
   trEl.appendChild(tdEl);
 
   storeTable.appendChild(trEl);
 };
 
-var allStores = [];
-
 new Store(stores[0], 23, 65, 6.3);
 new Store(stores[1], 3, 24, 1.2);
 new Store(stores[2], 11, 38, 3.7);
 new Store(stores[3], 20, 38, 2.3);
 new Store(stores[4], 2, 16, 4.6);
+console.table(Store.allStores);
 
-console.table(allStores);
 
 function makeHeaderRow() {
   var trEl = document.createElement('tr');
@@ -94,35 +93,69 @@ function makeHeaderRow() {
 }
 
 function makeStoreRows() {
-  for (var i = 0; i < allStores.length; i++) {
-    allStores[i].render();
+  for (var i = 0; i < Store.allStores.length; i++) {
+    Store.allStores[i].render();
   }
+}
+
+function makeFooterRow() {
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.className = 'salesHeadRow';
+  thEl.textContent = 'Hourly Totals for all Locations';
+  trEl.appendChild(thEl);
+
+  for (var i = 0; i < hours.length; i++) {
+    thEl = document.createElement('th');
+    thEl.className = 'salesHeadRow';
+    var allCookiesPerHour = 0;
+    var totalCookiesAllStores = 0;
+
+    for (var j = 0; j < Store.allStores.length; j++) {
+      allCookiesPerHour += Store.allStores[j].cookiesPerHour[i];
+      totalCookiesAllStores += Store.allStores[j].totalCookiesPerDay;
+      thEl.textContent = allCookiesPerHour;
+      trEl.appendChild(thEl);
+    }
+    
+  }
+  thEl = document.createElement('th');
+  thEl.className = 'salesHeadRow';
+  thEl.textContent = totalCookiesAllStores;
+  trEl.appendChild(thEl);
+  
+  storeTable.appendChild(trEl);
 }
 
 makeHeaderRow();
 makeStoreRows();
+makeFooterRow();
 
-//Form
-
-//Create global variables for DOM to access
 var entryForm = document.getElementById('entry');
 
-//Create event handler for sumbission of new entry
 function handleEntrySubmit(event) {
   event.preventDefault();
-  if (!event.target.avgCookiePC.value || !event.target.maxCustPH.value || !event.target.minCustPH.value ||!event.target.where.value) {
+
+  var where = event.target.where.value;
+  var minCustPH = parseInt(event.target.minCustPH.value);
+  var maxCustPH = parseInt(event.target.maxCustPH.value);
+  var avgCookiePC = parseFloat(event.target.avgCookiePC.value);
+
+  if (!avgCookiePC || !maxCustPH || !minCustPH ||!where) {
     return alert('Fields cannot be empty!');
   }
 
-  var newStore = new Store(event.target.where.value, event.target.minCustPH.value, event.target.maxCustPH.value, event.target.avgCookiePC.value);
+  new Store(where, minCustPH, maxCustPH, avgCookiePC);
+  storeTable.innerHTML = '';
 
-  event.target.where.value = null;
-  event.target.minCustPH.value = null;
-  event.target.maxCustPH.value = null;
-  event.target.avgCookiePC.value = null;
+  event.target.reset();
 
-  newStore.render();
+  makeHeaderRow();
+  makeStoreRows();
+  makeFooterRow();
+
+  console.table(Store.allStores);
 }
 
-//Event listener for entry submission form
 entryForm.addEventListener('submit', handleEntrySubmit);
+// console.table(Store.allStores);
